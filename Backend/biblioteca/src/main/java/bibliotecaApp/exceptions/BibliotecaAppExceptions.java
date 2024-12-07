1d46856d8da7aa91361adf99cc25677e830450b9
juanesHc <juanest006@gmail.com>
2024-12-06 22:43:23 -0500

    Exceptions en biblioteca

---
+++ Backend/biblioteca/src/main/java/bibliotecaApp/exceptions/BibliotecaAppExceptions.java
@@ -0,0 +1,35 @@
+package bibliotecaApp.exceptions;
+
+import crosscutting.exceptions.BibliotecaApplicationException;
+import crosscutting.exceptions.enums.Layer;
+
+public class BibliotecaAppExceptions extends BibliotecaApplicationException {
+
+    private static final long SerialVersionUID=1L;
+
+    public BibliotecaAppExceptions(final String userMessage,
+                                   final String technicalMessage,
+                                   final Exception rootException,
+                                   final Layer layer) {
+        super(userMessage, technicalMessage, rootException, layer);
+    }
+
+    public static BibliotecaAppExceptions create(final String userMessage,
+                                                       final String technicalMessage,
+                                                       final Exception rootException,
+                                                       final Layer layer){
+        return new BibliotecaAppExceptions(userMessage, technicalMessage, rootException, layer);
+
+    }
+    public static BibliotecaAppExceptions create(final String userMessage) {
+        return new BibliotecaAppExceptions(userMessage,userMessage,new Exception(),Layer.GENERAL);
+
+}
+
+    public static BibliotecaAppExceptions create(final String userMessage,final String technicalMessage) {
+        return new BibliotecaAppExceptions(userMessage,technicalMessage,new Exception(),Layer.GENERAL);
+
+    }
+
+
+}
